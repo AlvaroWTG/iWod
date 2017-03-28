ba712501ccf2d741aa1301c5ebe9369c2ed6da48
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2017-03-28 14:17:14 +0200

    - ViewController. Send synch request to parse html

--- iWod/Classes/ViewController.swift
+++ iWod/Classes/ViewController.swift
@@ -29,13 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //MARK: IBAction implementation method
+    //MARK: - IBAction implementation method
 
     @IBAction func didPressRefresh(_ sender: UIButton) {
         NSLog("Log: refreshing...")
     }
 
-    //MARK: Auxiliary functions
+    //MARK: - Auxiliary functions
+
+    /**
+     * Auxiliary function that parse initial content
+     */
+    func parse(content: String) {
+        let startString = "<h3 class=\"show\"><a href=\"/workout/2017/03/28\">"
+        var range = content.range(of: startString)
+        var stringBuilder = content.substring(from: (range?.lowerBound)!)
+        let endString = "</div>"
+        range = stringBuilder.range(of: endString)
+        stringBuilder = stringBuilder.substring(to: (range?.lowerBound)!)
+        DispatchQueue.main.async {self.labelWod.text = stringBuilder as String}
+    }
 
     /**
      * Auxiliary function that setups the interface
@@ -54,6 +67,9 @@ class ViewController: UIViewController {
         buttonRefresh.titleLabel?.text = "Refresh"
         labelDate.text = shareDate()
         labelWod.text = "Loading"
+
+        // Send synch request
+        sendSynchronousRequest(requestSession: "https://www.crossfit.com/workout/2017/03")
     }
 
     /**
@@ -63,6 +79,35 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateStyle = .full
         return formatter.string(from: Date())
+    }
+
+    /**
+     * Auxiliary function that obtains the reply to a request
+     * @param requestSession The request session that needs a reply
+     * @param isTest The boolean parameter to check whether there is a test
+     */
+    func sendSynchronousRequest(requestSession: String) {
+        var request = URLRequest.init(url: URL.init(string: requestSession)!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        if #available(iOS 9.0, *) {
+            let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let data = data, let result = String.init(data: data, encoding: String.Encoding.utf8) {
+                    self.parse(content: result)
+                } else if let error = error {
+                    NSLog("[NSURLConnection] Error! Found an error. Error 500: %@", error.localizedDescription)
+                }
+            })
+            task.resume()
+        } else { // Fallback on earlier versions
+            do {
+                var response: URLResponse?
+                let responseData = try NSURLConnection.sendSynchronousRequest(request, returning: &response)
+                let result = String.init(data: responseData, encoding: String.Encoding.utf8)
+                parse(content: result!)
+            } catch let error as NSError {
+                NSLog("[NSURLConnection] Error! Found an error. Error %d: %@", error.code, error.localizedDescription)
+            }
+        }
     }
 }
 
